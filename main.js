df41623d6912d1546f52b4e624f33e7f76fc268a
Ihor Rusyn <igarez20@gmail.com>
2023-01-21 22:16:52 +0200

    upd js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-function checkForSpam(message) {
-  let result;
+function calculateTotal(number) {
   // Change code below this line
-  if (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')) {
-    result = true;
-  } else {
-    result = false;
+  let sum = 0;
+  for (let i = 1; i <= number; i += 1) {
+    sum += i;
   }
+  return sum;
   // Change code above this line
-  return result;
 }
