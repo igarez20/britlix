175b364cacb06a18de2cfe6f11694f49ae0c26b7
Ihor Rusyn <igarez20@gmail.com>
2023-01-22 17:35:10 +0200

    js upd

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-function calculateTotal(number) {
+function filterArray(numbers, value) {
   // Change code below this line
-  let sum = 0;
-  for (let i = 1; i <= number; i += 1) {
-    sum += i;
-  }
-  return sum;
   // Change code above this line
 }
