9710d082f295f54efcd7827913692bf021a5aa32
igarez20 <igarez20@gmail.com>
2023-01-23 19:16:37 +0200

    upd task 23 solved

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 function filterArray(numbers, value) {
   // Change code below this line
+  const newArray = [];
+
+  for (let i = 0; numbers.length > i; i += 1) {
+    if (numbers[i] > value) {
+      newArray.push(numbers[i]);
+    }
+  }
+
+  return newArray;
   // Change code above this line
 }
