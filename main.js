709721c97c03e3a9f574c49cf255fc5789461fb0
Ihor Rusyn <igarez20@gmail.com>
2023-01-26 11:50:57 +0200

    m2 task 32 complete

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function getEvenNumbers(start, end) {
+function includes(array, value) {
   // Change code below this line
-  const evenNumbers = [];
+  for (const item of array) {
+    if (item === value) {
+      return true;
+    }
+  }
+  return false;
 
   // Change code above this line
 }
