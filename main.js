c2315147528ec2d11692d41b465172cf6490effc
Ihor Rusyn <igarez20@gmail.com>
2023-02-09 19:43:50 +0200

    task 24

---
+++ main.js
@@ -0,0 +1,8 @@
+// // Change code below this line
+// const getUsersWithFriend = (users, friendName) =>
+//   users.filter(user => user.friends.includes(friendName));
+// // Change code above this line
+
+// // Change code below this line
+// const getFriends = users => users.filter(user => user.friends);
+// // Change code above this line
