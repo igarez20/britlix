196410b762227578f90b68d7b4d16ea4cb23e87a
Ihor Rusyn <igarez20@gmail.com>
2023-02-10 20:38:11 +0200

    tasks up to 38

--- main.js
+++ main.js
@@ -1,64 +1,20 @@
-// const books = [
-//   {
-//     title: 'The Last Kingdom',
-//     author: 'Bernard Cornwell',
-//     rating: 8.38,
-//   },
-//   {
-//     title: 'Beside Still Waters',
-//     author: 'Robert Sheckley',
-//     rating: 8.51,
-//   },
-//   {
-//     title: 'The Dream of a Ridiculous Man',
-//     author: 'Fyodor Dostoevsky',
-//     rating: 7.75,
-//   },
-//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
-// ];
-// const BOOK_TITLE = 'The Dream of a Ridiculous Man';
-// const AUTHOR = 'Robert Sheckley';
-// // Change code below this line
+const players = [
+  { name: 'Mango', playtime: 1270, gamesPlayed: 4 },
+  { name: 'Poly', playtime: 469, gamesPlayed: 2 },
+  { name: 'Ajax', playtime: 690, gamesPlayed: 3 },
+  { name: 'Kiwi', playtime: 241, gamesPlayed: 1 },
+];
+// Change code below this line
 
-// const bookWithTitle = books.find(book => book.title === BOOK_TITLE);
-// const bookByAuthor = books.find(book => book.author === AUTHOR);
+const totalAveragePlaytimePerGame = players.reduce(
+  (avarage, player) => avarage + player.playtime / player.gamesPlayed,
+  0
+);
 
-// // Change code below this line
-// const getUserWithEmail = (users, email) => users.find(user => user.email === email);
-// // Change code above this line
+// Change code below this line
+const calculateTotalBalance = users => users.reduce((total, user) => total + user.balance, 0);
+// Change code above this line
 
-// const firstArray = [26, 94, 36, 18];
-// const secondArray = [17, 61, 23];
-// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
-// // Change code below this line
-
-// const eachElementInFirstIsEven = firstArray.every(number => number % 2 === 0);
-// const eachElementInFirstIsOdd = firstArray.every(number => number % 2 !== 0);
-
-// const eachElementInSecondIsEven = secondArray.every(number => number % 2 === 0);
-// const eachElementInSecondIsOdd = secondArray.every(number => number % 2 !== 0);
-
-// const eachElementInThirdIsEven = thirdArray.every(number => number % 2 === 0);
-// const eachElementInThirdIsOdd = thirdArray.every(number => number % 2 !== 0);
-
-// // Change code below this line
-// const isEveryUserActive = users => users.every(user => user.isActive === true);
-// // Change code above this line
-
-// const firstArray = [26, 94, 36, 18];
-// const secondArray = [17, 61, 23];
-// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
-// // Change below this line
-
-// const anyElementInFirstIsEven = firstArray.some(number => number % 2 === 0);
-// const anyElementInFirstIsOdd = firstArray.some(number => number % 2 !== 0);
-
-// const anyElementInSecondIsEven = secondArray.some(number => number % 2 === 0);
-// const anyElementInSecondIsOdd = secondArray.some(number => number % 2 !== 0);
-
-// const anyElementInThirdIsEven = thirdArray.some(number => number % 2 === 0);
-// const anyElementInThirdIsOdd = thirdArray.some(number => number % 2 !== 0);
-
-// // Change code below this line
-// const isAnyUserActive = users => users.some(user => user.isActive === true);
-// // Change code above this line
+// Change code below this line
+const getTotalFriendCount = users => users.reduce((count, user) => count + user.friends.length, 0);
+// Change code above this line
