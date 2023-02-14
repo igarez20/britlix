53fef8c28556e902b015372fe6de6044b62e55c8
Ihor Rusyn <igarez20@gmail.com>
2023-02-14 21:22:32 +0200

    tasks up to 48

--- main.js
+++ main.js
@@ -51,3 +51,57 @@ const sortByDescendingFriendCount = users =>
 const sortByName = users =>
   [...users].sort((curElement, nextElement) => curElement.name.localeCompare(nextElement.name));
 // Change code above this line
+
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  {
+    title: 'The Dreams in the Witch House',
+    author: 'Howard Lovecraft',
+    rating: 8.67,
+  },
+];
+const MIN_BOOK_RATING = 8;
+// Change code below this line
+
+const names = [...books]
+  .filter(book => book.rating > MIN_BOOK_RATING)
+  .sort((curElement, nextElement) => curElement.author.localeCompare(nextElement.author))
+  .map(bookAuthor => bookAuthor.author);
+
+// Change code below this line
+const getNamesSortedByFriendCount = users =>
+  [...users]
+    .sort((curElement, nextElement) => curElement.friends.length - nextElement.friends.length)
+    .map(userName => userName.name);
+// Change code above this line
+
+// Change code below this line
+const getSortedFriends = users =>
+  [...users]
+    .flatMap(userFriends => userFriends.friends)
+    .filter((friend, index, array) => array.indexOf(friend) === index)
+    .sort();
+// Change code above this line
+
+// Change code below this line
+const getTotalBalanceByGender = (users, gender) =>
+  [...users]
+    .filter(user => user.gender === gender)
+    .reduce((total, user) => total + user.balance, 0);
+
+// Change code above this line
