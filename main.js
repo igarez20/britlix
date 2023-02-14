be2da9301710b8d62b9c0efd89e1dbd3b6a27750
Ihor Rusyn <igarez20@gmail.com>
2023-02-14 20:26:48 +0200

    tasks up to 44

--- main.js
+++ main.js
@@ -1,20 +1,53 @@
-const players = [
-  { name: 'Mango', playtime: 1270, gamesPlayed: 4 },
-  { name: 'Poly', playtime: 469, gamesPlayed: 2 },
-  { name: 'Ajax', playtime: 690, gamesPlayed: 3 },
-  { name: 'Kiwi', playtime: 241, gamesPlayed: 1 },
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
 ];
 // Change code below this line
 
-const totalAveragePlaytimePerGame = players.reduce(
-  (avarage, player) => avarage + player.playtime / player.gamesPlayed,
-  0
+const sortedByAuthorName = [...books].sort((curElement, nextElement) =>
+  curElement.author.localeCompare(nextElement.author)
+);
+
+const sortedByReversedAuthorName = [...books].sort((curElement, nextElement) =>
+  nextElement.author.localeCompare(curElement.author)
+);
+
+const sortedByAscendingRating = [...books].sort(
+  (curElement, nextElement) => curElement.rating - nextElement.rating
+);
+
+const sortedByDescentingRating = [...books].sort(
+  (curElement, nextElement) => nextElement.rating - curElement.rating
 );
 
 // Change code below this line
-const calculateTotalBalance = users => users.reduce((total, user) => total + user.balance, 0);
+const sortByAscendingBalance = users =>
+  [...users].sort((curElement, nextElement) => curElement.balance - nextElement.balance);
 // Change code above this line
 
 // Change code below this line
-const getTotalFriendCount = users => users.reduce((count, user) => count + user.friends.length, 0);
+const sortByDescendingFriendCount = users =>
+  [...users].sort(
+    (curElement, nextElement) => nextElement.friends.length - curElement.friends.length
+  );
+// Change code above this line
+
+// Change code below this line
+const sortByName = users =>
+  [...users].sort((curElement, nextElement) => curElement.name.localeCompare(nextElement.name));
 // Change code above this line
